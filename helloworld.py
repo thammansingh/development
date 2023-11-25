c65b3adf24e541209d35b50b26ed3080efe2f267
thammansingh <meetthamman@gmail.com>
2023-11-25 13:49:53 +0530

    first commit to add tow number

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+
+# WRITE A PROGRAMME TO INPUT Two numbers  AND ADD and print them
+print('Program of sum of tw numbers')
+num1 = int(input('Enter the First Number  '))
+num2 = int(input('Enter the Second Number  '))
+result = num1 + num2
+print(f 'Sum of two numbers are  {result}')
+
+print("Hello world")
